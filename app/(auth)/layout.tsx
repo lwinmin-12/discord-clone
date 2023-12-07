8fb6b61fb8774921413008f68e00fb1ef7a2ec88
MAC <mac@LMOs-MacBook-Pro.local>
2023-12-07 23:12:20 +0630

    darkAndLight icon update

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen flex items-center justify-center ">
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
